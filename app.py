30a98c1dab62da25b182a93fcb1848fd397c823d
SoursosK <kon.soursos_@hotmail.com>
2020-01-23 12:32:52 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from tkinter import Tk
+from tkinter import filedialog
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+from os import chdir
+import socket
+from multiprocessing.pool import ThreadPool
+import time
+
+def selectFolder():
+    Tk().withdraw()
+    folder_selected = filedialog.askdirectory()
+    chdir(folder_selected)
+
+def serve():
+    httpd = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    print('The server will be running at ' + socket.gethostbyname(socket.gethostname()) + ' for 2 minutes!\n')
+    httpd.serve_forever()
+
+
+def main():
+    selectFolder()
+
+    pool = ThreadPool(processes=1)
+    pool.apply_async(serve)
+    time.sleep(120)
+   
+    pool.terminate()
+    
+
+if __name__ == '__main__':
+    main()
+
+
+
